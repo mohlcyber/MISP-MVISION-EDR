f1fdcbcb5e297e223eccdc84aa5bb9e2d2de8d63
Martin Ohl <mohl@Martins-MacBook-Pro.local>
2020-06-08 17:36:40 +0200

    v.0.1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This integration adds automated hunting capabilities to the MISP platform with M
 Based on tagging a script will extract suspicious MD5 hashes from a threat event and will launch automated MVISION EDR lookups. 
 If indicators found - the script will automatically re-tag the threat event, add sightings, add attributes and comments.
 
-<img width="863" alt="Screenshot 2020-02-13 at 20 42 35" src="https://user-images.githubusercontent.com/25227268/74471958-6a661e80-4ea1-11ea-89f7-0c11356b2024.png">
+<img width="863" alt="Screenshot 2020-02-13 at 20 42 35" src="https://user-images.githubusercontent.com/25227268/74471958-6a661e80-4ea1-11ea-89f7-0c11356b2024.png"> 
 
 ## Component Description
 **MISP** threat sharing platform is a free and open source software helping information sharing of threat and cyber security indicators. https://github.com/MISP/MISP
@@ -14,7 +14,13 @@ If indicators found - the script will automatically re-tag the threat event, add
 **McAfee MVISION EDR** is an endpoint detection and response solution. It provides the cability to query endpoint in real-time. https://www.mcafee.com/enterprise/en-us/products/mvision-edr.html
 
 ## Prerequisites
-MISP platform ([Link](https://github.com/MISP/MISP)) (tested with MISP 2.4.121)
+This repo includes two different ways to integrate MISP with McAfee MVISION EDR. 
+
+**misp_edr.py** integrates using the API's.
+
+**misp_dxl.py** integrates using OpenDXL and can be used for McAfee Active Response as well.
+
+### misp_edr.py
 
 PyMISP ([Link](https://github.com/MISP/PyMISP))
 ```sh
@@ -23,7 +29,16 @@ cd PyMISP/
 python setup.py install
 ```
 
-Requests ([Link](http://docs.python-requests.org/en/master/user/install/#install))
+Access to MVISION EDR Tenant
+
+### misp_dxl.py
+
+PyMISP ([Link](https://github.com/MISP/PyMISP))
+```sh
+git clone https://github.com/MISP/PyMISP.git
+cd PyMISP/
+python setup.py install
+```
 
 OpenDXL SDK ([Link](https://github.com/opendxl/opendxl-client-python))
 ```sh
@@ -41,29 +56,52 @@ python setup.py install
 
 On-Prem McAfee ePolicy Orchestrator, DXL Broker, MVISION EDR.
 
-## Configuration
-Enter the MISP url and access key in the misp_edr.py file (line 16, 17).
-
-Create a tag that the analyst uses to initiate the hunting process. (e.g. McAfee: Run MVISION EDR Query).
-
-Create a tag that will be assigned to event where indicators found. (e.g. McAfee: MVISION EDR Indicator Found).
-
-Modify the misp_edr.py file (line 16 - 22).
-```sh
-misp_url = 'https://1.1.1.1'
-misp_key = 'api key'
-misp_verify = False
-misp_tag = 'McAfee: Run MVISION EDR Query'
-misp_ntag = 'McAfee: MVISION EDR Indicator Found'
-
-dxl_config = '/path/to/dxlclient.config'
-```
 Create Certificates for OpenDXL and move them into the config folder ([Link](https://opendxl.github.io/opendxl-client-python/pydoc/epoexternalcertissuance.html)). 
 
 Make sure to authorize the new created certificates in ePO to use the McAfee Active Response API ([Link](https://opendxl.github.io/opendxl-client-python/pydoc/marsendauth.html)).
 
 Make sure that the FULL PATH to the config file is entered in line 22.
 
+## Configuration
+Enter the MISP url and access key in the python file.
+
+Create a tag that the analyst uses to initiate the hunting process. (e.g. McAfee: Run MVISION EDR Query).
+
+Create a tag that will be assigned to event where indicators found. (e.g. McAfee: MVISION EDR Indicator Found).
+
+Modify the python file.
+```sh
+misp_url = 'https://1.1.1.1'
+misp_key = ''
+misp_verify = False
+misp_tag = 'McAfee: Run MVISION EDR Query'
+misp_ntag = 'McAfee: MVISION EDR Indicator Found'
+```
+
+## Run the script
+
+### misp_edr.py
+
+```
+usage: python misp_edr.py -R <REGION> -U <USERNAME> 
+
+MISP - McAfee EDR Integration
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --region {EU,US}, -R {EU,US}
+                        MVISION EDR Tenant Location
+  --user USER, -U USER  MVISION EDR Username
+  --password PASSWORD, -P PASSWORD
+                        MVISION EDR Password
+
+```
+
+### misp_dxl.py
+
+```
+python3 misp_dxl.py
+```
 ## Video
 
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/C68cJ9XnjbI/0.jpg)](https://youtu.be/C68cJ9XnjbI)
--- misp_edr.py
+++ misp_edr.py
@@ -1,35 +1,138 @@
 #!/usr/bin/env python3
-# Written by mohlcyber v.0.3 06/03/2020
+# Written by mohlcyber v.0.1 08/06/2020
 
 import requests
 import sys
 import time
+import logging
+import getpass
 
+from argparse import ArgumentParser, RawTextHelpFormatter
 from pymisp import ExpandedPyMISP, MISPAttribute
-
-from dxlclient.client import DxlClient
-from dxlclient.client_config import DxlClientConfig
-from dxlmarclient import MarClient
 
 requests.packages.urllib3.disable_warnings()
 
 misp_url = 'https://1.1.1.1'
-misp_key = 'api key'
+misp_key = ''
 misp_verify = False
 misp_tag = 'McAfee: Run MVISION EDR Query'
 misp_ntag = 'McAfee: MVISION EDR Indicator Found'
-
-dxl_config = 'path to dxlclient.config'
 
 
 class EDRMISP():
 
     def __init__(self):
-        self.config = DxlClientConfig.create_dxl_config_from_file(dxl_config)
         self.misp = ExpandedPyMISP(misp_url, misp_key, misp_verify)
         self.tags = self.misp.tags()
         self.attributes = []
         self.found = False
+
+        if args.region == 'EU':
+            self.edr = 'https://api.soc.eu-central-1.mcafee.com/'
+        elif args.region == 'US':
+            self.edr = 'https://api.soc.mcafee.com/'
+
+        self.verify = True
+        self.session = requests.Session()
+
+        user = args.user
+        pw = args.password
+        self.creds = (user, pw)
+
+    def edr_auth(self):
+        r = self.session.get(self.edr + '/identity/v1/login', auth=self.creds)
+        res = r.json()
+
+        if r.status_code == 200:
+            token = res['AuthorizationToken']
+            self.headers = {'Authorization': 'Bearer {}'.format(token)}
+            print('AUTHENTICATION: Successfully authenticated.')
+        else:
+            print('ERROR: Something went wrong during the authentication')
+            sys.exit()
+
+    def edr_search(self, hash):
+        payload = {
+            "projections": [
+                {
+                    "name": "HostInfo",
+                    "outputs": ["hostname", "ip_address"]
+                }, {
+                    "name": "Files",
+                    "outputs": ["name", "md5", "status", "full_name"]
+                }
+            ],
+            "condition": {
+                "or": [{
+                    "and": [{
+                        "name": "Files",
+                        "output": "md5",
+                        "op": "EQUALS",
+                        "value": str(hash)
+                    }]
+                }]
+            }
+        }
+
+        res = self.session.post(self.edr + '/active-response/api/v1/searches',
+                                headers=self.headers,
+                                json=payload)
+        if res.status_code == 200:
+            queryId = res.json()['id']
+            print('SEARCH: MVISION EDR search got started successfully')
+        else:
+            print('ERROR: Could not find the query ID.')
+            sys.exit()
+
+        return queryId
+
+    def edr_status(self, queryId):
+        status = False
+        res = self.session.get(self.edr + '/active-response/api/v1/searches/{}/status'.format(str(queryId)),
+                               headers=self.headers)
+        if res.status_code == 200:
+            if res.json()['status'] == 'FINISHED':
+                status = True
+            else:
+                print('STATUS: Search still in process. Status: {}'.format(res.json()['status']))
+        return status
+
+    def edr_result(self, queryId, eventid, attr_id, attr_uuid):
+        hostnames = []
+        res = self.session.get(self.edr + '/active-response/api/v1/searches/{}/results'.format(str(queryId)),
+                               headers=self.headers)
+        if res.status_code == 200:
+            if res.json()['items'] != []:
+                total = res.json()['totalItems']
+                print('SUCCESS: Found {0} Host(s) with this hash.'.format(str(total)))
+
+                for item in res.json()['items']:
+                    hostname = item['output']['HostInfo|hostname']
+                    ip = item['output']['HostInfo|ip_address']
+                    status = item['output']['Files|status']
+                    full_name = item['output']['Files|full_name']
+                    md5 = item['output']['Files|md5']
+                    finding = 'Hostname: {0} | IP: {1} | Status: {2} | Location: {3} | MD5: {4}'\
+                        .format(hostname, ip, status, full_name, md5)
+                    hostnames.append(hostname)
+
+                    self.found = True
+                    self.add_sighting(attr_id, hostname)
+                    self.add_attribute(eventid, finding)
+
+                self.update_attribute(attr_id, hostnames, attr_uuid)
+            else:
+                print('SUCCESS: No System found containing files with hash.')
+        else:
+            print('ERROR: Something went wrong to retrieve the results.')
+            sys.exit()
+
+    def edr_run_search(self, eventid, hash, attr_id, attr_uuid):
+        self.edr_auth()
+        queryid = self.edr_search(hash)
+        while self.edr_status(queryid) is False:
+            time.sleep(10)
+        self.edr_result(queryid, eventid, attr_id, attr_uuid)
 
     def add_attribute(self, eventid, finding):
         attr = {
@@ -53,58 +156,6 @@ class EDRMISP():
         }
         self.misp.add_sighting(sight)
 
-    def edr_search(self, eventid, hash, attr_id, attr_uuid):
-        hostnames = []
-
-        with DxlClient(self.config) as client:
-            client.connect()
-            marclient = MarClient(client)
-
-            results_context = \
-                marclient.search(
-                    projections=[{
-                        "name": "HostInfo",
-                        "outputs": ["hostname", "ip_address"]
-                    }, {
-                        "name": "Files",
-                        "outputs": ["name", "md5", "status", "full_name"]
-                    }],
-                    conditions={
-                        "or": [{
-                            "and": [{
-                                "name": "Files",
-                                "output": "md5",
-                                "op": "EQUALS",
-                                "value": hash
-                            }]
-                        }]
-                    }
-                )
-
-            if results_context.has_results:
-                results = results_context.get_results()
-                total = results['totalItems']
-                print('SUCCESS: Found {0} Host(s) with hash {1}.'.format(str(total), hash))
-
-                for item in results['items']:
-                    hostname = item['output']['HostInfo|hostname']
-                    ip = item['output']['HostInfo|ip_address']
-                    status = item['output']['Files|status']
-                    full_name = item['output']['Files|full_name']
-                    md5 = item['output']['Files|md5']
-                    finding = 'Hostname: {0} | IP: {1} | Status: {2} | Location: {3} | MD5: {4}'\
-                        .format(hostname, ip, status, full_name, md5)
-                    hostnames.append(hostname)
-
-                    self.found = True
-                    self.add_sighting(attr_id, hostname)
-                    self.add_attribute(eventid, finding)
-
-                self.update_attribute(attr_id, hostnames, attr_uuid)
-
-            else:
-                print('SUCCESS: No System found containing files with hash {0}'.format(hash))
-
     def main(self):
         try:
             events = self.misp.search(tags=misp_tag)
@@ -115,14 +166,15 @@ class EDRMISP():
                         if attributes['type'] == 'md5':
                             print('STATUS: Found MD5 {0} in Event {1}. Trying to lookup Endpoint with MVISION EDR.'
                                   .format(str(attributes['value']), eventid))
-                            self.edr_search(eventid, attributes['value'], attributes['id'], attributes['uuid'])
+
+                            self.edr_run_search(eventid, attributes['value'], attributes['id'], attributes['uuid'])
 
                     for objects in event['Event']['Object']:
                         for attributes in objects['Attribute']:
                             if attributes['type'] == 'md5':
                                 print('STATUS: Found MD5 {0} in Event {1}. Trying to lookup Endpoint with MVISION EDR.'
                                       .format(str(attributes['value']), eventid))
-                                self.edr_search(eventid, attributes['value'], attributes['id'], attributes['uuid'])
+                                self.edr_run_search(eventid, attributes['value'], attributes['id'], attributes['uuid'])
 
                     self.misp.untag(event['Event']['uuid'], misp_tag)
 
@@ -137,6 +189,27 @@ class EDRMISP():
 
 
 if __name__ == '__main__':
+    usage = """python misp_edr.py -R <REGION> -U <USERNAME> """
+    title = 'MISP - McAfee EDR Integration'
+    parser = ArgumentParser(description=title, usage=usage, formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('--region', '-R',
+                        required=True, type=str,
+                        help='MVISION EDR Tenant Location', choices=['EU', 'US'])
+
+    parser.add_argument('--user', '-U',
+                        required=True, type=str,
+                        help='MVISION EDR Username')
+
+    parser.add_argument('--password', '-P',
+                        required=False, type=str,
+                        help='MVISION EDR Password')
+
+    args = parser.parse_args()
+    if not args.password:
+        args.password = getpass.getpass(prompt='MVISION EDR Password:')
+
+    print('STATUS: Starting the MISP - MVISION EDR Service.')
     while True:
         misp = EDRMISP().main()
         time.sleep(30)
